38b0601c4702174a966e2e659d7fa5c407a56836
BunniMK <86545296+BunniMK@users.noreply.github.com>
2022-01-13 12:12:11 -0700

    creating react app

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const user = process.env.USERDOMAIN
+require('dotenv').config()
+const path = require('path')
 
 if ( process.argv[2] === 'web-49'){
     console.log(user)
@@ -9,6 +11,9 @@ if ( process.argv[2] === 'web-49'){
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+app.use(express.static(path.join(_dirname, 'client/build', 'index.html')))
+
 app.get('/hello', (req, res) => {
     res.json({ message: "fahook"})
 })
@@ -18,6 +23,9 @@ app.get('/', (req, res) => {
     `<h1> hello there </h1>`
     )
 })
+
+app.get( '*', (req, res) => {
+    path.join(_dirname, 'client/build', 'index.html')})
 
 const port = process.env.PORT || 9000
 
